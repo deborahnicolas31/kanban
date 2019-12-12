e746ebee183505624b1761464b75333f2a5631f9
déborah nicolas <deborah.nicolas@etud.univ-jfc.fr>
2019-12-12 16:21:01 +0100

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { ListComponent } from './list/list.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    ListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/board/board.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { CardStore } from '../CardStore';
+import { ListSchema } from '../ListSchema';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+  cardStore: CardStore;
+  lists: ListSchema[];
+  constructor() { }
+  setMockData(): void {
+    this.cardStore = new CardStore();
+    const lists: ListSchema[] = [
+      {
+        name: 'À faire',
+        cards: []
+      },
+      {
+        name: 'En cours',
+        cards: []
+      },
+      {
+        name: 'Terminé',
+        cards: []
+      },
+      {
+        name: 'Archivé',
+        cards: []
+      }
+    ];
+    this.lists = lists;
+  }
+
+  ngOnInit() {
+    this.setMockData();
+  }
+
+}
